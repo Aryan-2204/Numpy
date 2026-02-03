0d55bcf2f322ecc2de193dc0c82a0afc1772683d
Aryxnnn <akadam2204@gmail.com>
2026-02-03 23:34:40 +0530

    over with more functions on numpy

--- numpy_tricks.py
+++ numpy_tricks.py
@@ -34,10 +34,61 @@ print(np.argmax(a)) #gives index of maximum element
 print(np.argmax(b,axis=0)) #gives index of maximum element column wise
 
 #argmin()
-
-#cumsum()
-
-
+print(np.argmin(a)) #gives index of minimum element
+print(np.argmin(b,axis=1)) #gives index of minimum element row wise
 
 
+#cumsum()- cumulative sum
+print(np.cumsum(b,axis=0)) #cumulative sum of elements in array    
 
+#cumprod()
+print(np.cumprod(b,axis=1))
+
+#percentile
+print(np.percentile(a,100))
+
+#histogram- gives frequency count in given range
+print(np.histogram(a,bins=[0,10,20,30,40,50,60,70,80,90,100]))
+
+
+
+#corrcoef- {how much the two qauntities are co-related}
+salary=np.array([2000,3000,4000,5000,6000])
+experience=np.array([1,2,3,4,5,])
+print(np.corrcoef(salary,experience))
+
+#isin
+items=[10,20,30,40,50]
+print(np.isin(a,items))# to find if items are present in array (a)
+
+#flips
+print(np.flip(a)) #creates a mirror image works on 2D as well
+
+print(np.flip(b)) #flips row and column but can do singly by proviidng axis
+
+#put (edits in array)
+print(np.put(a,[0,1],[10,40]))
+
+#delete (returns new array after deletion)
+
+print(np.delete(a,0))
+
+
+#set functions
+
+#union
+m= np.array([1,2,3,4,5,6,7,4])
+n=np.array([1,3,4,5,67,8,4])
+
+print(np.union1d(m,n))
+
+print(np.intersect1d(m,n))
+
+print(np.setdiff1d(m,n)) # present in m but not n
+
+print(np.setxor1d(m,n)) #removes common
+
+#print(np.in1d(m,10))# searches if present in m
+
+#clip - keeps values in range
+print(np.clip(a,a_min=35,a_max=75))
